10a77e9b36c9c4668303cdc03d6a4e824e7fa9f3
Parefair <kanyaluck.cc@gmail.com>
2022-08-14 15:46:44 +0700

    use dai laew ka

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ client.once('ready', account => {
     console.log(`Your Deary is Ready !! Login as ${account.user.tag}`);
 });
 
-client.on('interactionCreate', interaction => {
-	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-});
+// client.on('interactionCreate', interaction => {
+// 	console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+// });
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  });
 
 client.login(process.env.TOKEN);  
